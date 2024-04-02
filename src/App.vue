330640705680b425dacffb4a930e96ce952c8d3e
Qiu-Quanzhi <i@qqzhi.cc>
2024-04-02 18:17:46 +0800

    rename

--- src/App.vue
+++ src/App.vue
@@ -97,7 +97,7 @@ const changeLang = (lang: string) => {
             <p class="workLinkIntro">{{ t('parts.work.contents[0].intro') }}</p>
             <p class="workLinkCat">{{ t('parts.work.contents[0].cat') }}</p>
           </div>
-        </a><a href="AncientMap/" target="_blank" class="workLink" id="workLink_History">
+        </a><a href="HistoryMap/" target="_blank" class="workLink" id="workLink_History">
           <div class="workLinkPic"></div>
           <div class="workLinkText">
             <p class="workLinkTitle">{{ t('parts.work.contents[1].title') }}</p>
