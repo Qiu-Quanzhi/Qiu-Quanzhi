cb747f6c2e519687cb410cefa3c7f17dc792528a
Qiu-Quanzhi <i@qqzhi.cc>
2024-04-03 02:17:44 +0800

    HistoryMap publish

--- docs/works/HistoryMap/index.js
+++ docs/works/HistoryMap/index.js
@@ -17,6 +17,6 @@ function enter() {
 	document.getElementById("sidebar-mask").setAttribute("class", "sidebar mask")
 	document.getElementById("sidebar").setAttribute("class", "sidebar")
 	setTimeout(function() {
-		window.location.href = "//historyApp.qqzhi.cc"
+		window.location.href = "https://app.qqzhi.cc/HistoryApp"
 	}, 400)
 }
