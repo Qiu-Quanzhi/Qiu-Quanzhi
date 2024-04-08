407c494db2cef46aa495890efed3f74766b387d5
Qiu-Quanzhi <i@qqzhi.cc>
2024-04-08 21:36:10 +0800

    test compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: './'
+  plugins: [vue(),
+    viteCompression({
+      algorithm: 'brotliCompress',
+      deleteOriginFile: true
+    })
+  ],
+  base: './',
+  build:{
+    target: 'esnext',
+    modulePreload:{
+      polyfill: false
+    },
+    reportCompressedSize: false
+  }
 })
