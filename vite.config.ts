e3ad259d206bc6ec702bc64b0f61ec85868bb46b
Qiu-Quanzhi <i@qqzhi.cc>
2024-04-12 20:52:24 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-import compression from 'vite-plugin-compression'
+//import compression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    compression({
-      algorithm: "brotliCompress",
-      ext: ".br",
-      threshold: 0
-    }
-    )],
+    // compression({
+    //   algorithm: "brotliCompress",
+    //   ext: ".br",
+    //   threshold: 0
+    // })
+  ],
   base: './',
   build: {
     rollupOptions: {
