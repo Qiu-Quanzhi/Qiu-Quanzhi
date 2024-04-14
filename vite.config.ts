eae4ac14cbbb0863c5825b71ea4e0e2093c0dfde
Qiu-Quanzhi <i@qqzhi.cc>
2024-04-14 18:24:25 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-//import compression from 'vite-plugin-compression'
+import CSP from 'vite-plugin-csp'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    // compression({
-    //   algorithm: "brotliCompress",
-    //   ext: ".br",
-    //   threshold: 0
-    // })
+    vue(),CSP()
   ],
   base: './',
   build: {
