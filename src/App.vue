8e9d5c0b2d8b72f0e1f3725127c6f77570472ecb
旅禾Ryoine <i@qqzhi.cc>
2025-02-18 02:23:57 +0800

    添加 规范链接并优化语言切换时的处理逻辑

--- src/App.vue
+++ src/App.vue
@@ -69,18 +69,8 @@ const changeLang = (lang: string,event?: MouseEvent) => {
   document.documentElement.lang = t('lang')
   document.title = t('title')
   history.replaceState(null, '', t('href'))
-
-  var canonicalLink = document.querySelector('link[rel="canonical"]');
-  if (canonicalLink) {
-    canonicalLink.setAttribute('href', location.href);
-  } else {
-    var newLink = document.createElement('link');
-    newLink.setAttribute('rel', 'canonical');
-    newLink.setAttribute('href', location.href);
-    document.head.appendChild(newLink);
-  }
+  document.querySelector('link[rel="canonical"]')?.setAttribute('href', location.href)
 }
-
 onMounted(async () => {
   location.pathname !== t('href') && changeLang(t('href').replace('/',''))
   setTimeout(() => {
