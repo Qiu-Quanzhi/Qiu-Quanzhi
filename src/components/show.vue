27d312e01a2210179fdff01f603a763a4a461b37
Qiu-Quanzhi <i@qqzhi.cc>
2026-06-23 15:17:20 +0800

    refactor(show): tabs 用 v-for + 动态组件消除重复，减少 if-else 分支

--- src/components/show.vue
+++ src/components/show.vue
@@ -2,7 +2,7 @@
 import blog from './show/blog.vue'
 import bilibili from './show/bilibili.vue'
 import netease from './show/netease.vue'
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 const { t } = useI18n();
 type BilibiliItem = { aid?: number, bvid?: string, cid?: number, title: string }
@@ -21,24 +21,23 @@ fetch('/assets/data/works.json')
         list.value = { bilibili_list: [] };
     })
 const bilibili_index = ref(-1)
-const tab=ref('blog')
+const tab = ref('blog')
+
+const showTabs = [
+    { id: 'bilibili', icon: 'assets/icons/Bilibili.svg', href: 'https://space.bilibili.com/1036651852', ariaKey: 'aria.Bilibili', nameKey: 'parts.show.tabs.bilibili.name', enterKey: 'parts.show.tabs.bilibili.enter' },
+    { id: 'blog', icon: 'assets/icons/blog.png', href: 'https://home.qqzhi.cc/', ariaKey: 'aria.blog', nameKey: 'parts.show.tabs.blog.name', enterKey: 'parts.show.tabs.blog.enter' },
+    { id: 'netease', icon: 'assets/icons/Netease.svg', href: 'https://music.163.com/#/artist?id=55151766', ariaKey: 'aria.Netease', nameKey: 'parts.show.tabs.netease.name', enterKey: 'parts.show.tabs.netease.enter' },
+]
+
+const tabComponents: Record<string, any> = { bilibili, blog, netease }
+const currentTabComponent = computed(() => tabComponents[tab.value])
 </script>
 <template>
     <view class="selection-box flex-row">
-        <view @click="tab='bilibili'" class="flex-col item-center">
-            <img :alt="t('aria.Bilibili')" :class="['selection',tab=='bilibili'?'selected':'']" :src="`assets/icons/Bilibili.svg`"/>
-            <span v-show="tab=='bilibili'" class="underline2"></span>
-            <a target="_blank" href="https://space.bilibili.com/1036651852" :class="['selection-id',tab=='bilibili'?'selected':'']">{{ t('parts.show.tabs.bilibili.name') }}<br><text>({{ t('parts.show.tabs.bilibili.enter') }})</text></a>
-        </view>
-        <view @click="tab='blog'" class="flex-col item-center">
-            <img :alt="t('aria.blog')" :class="['selection',tab=='blog'?'selected':'']" :src="`assets/icons/blog.png`"/>
-            <span v-show="tab=='blog'" class="underline2"></span>
-            <a target="_blank" href="https://home.qqzhi.cc/" :class="['selection-id',tab=='blog'?'selected':'']">{{ t('parts.show.tabs.blog.name') }}<br><text>({{ t('parts.show.tabs.blog.enter') }})</text></a>
-        </view>
-        <view @click="tab='netease'" class="flex-col item-center">
-            <img :alt="t('aria.Netease')" :class="['selection',tab=='netease'?'selected':'']" :src="`assets/icons/Netease.svg`"/>
-            <span v-show="tab=='netease'" class="underline2"></span>
-            <a target="_blank"  href="https://music.163.com/#/artist?id=55151766" :class="['selection-id',tab=='netease'?'selected':'']">{{ t('parts.show.tabs.netease.name') }}<br><text>({{ t('parts.show.tabs.netease.enter') }})</text></a>
+        <view v-for="tabItem in showTabs" :key="tabItem.id" @click="tab = tabItem.id" class="flex-col item-center">
+            <img :alt="t(tabItem.ariaKey)" :class="['selection', tab == tabItem.id ? 'selected' : '']" :src="tabItem.icon"/>
+            <span v-show="tab == tabItem.id" class="underline2"></span>
+            <a target="_blank" :href="tabItem.href" :class="['selection-id', tab == tabItem.id ? 'selected' : '']">{{ t(tabItem.nameKey) }}<br><text>({{ t(tabItem.enterKey) }})</text></a>
         </view>
     </view>
     <view v-if="tab=='bilibili'" class="show-box flex-row content-center" lang="zh-CN">
@@ -47,18 +46,15 @@ const tab=ref('blog')
                 :bvid="list.bilibili_list[bilibili_index].bvid" :cid="list.bilibili_list[bilibili_index].cid"></bilibili>
         </view>
         <view class="list flex-col">
-            
+
             <view @click="bilibili_index = index" :class="['list-item', index == bilibili_index ? 'selected' : '']"
                 v-for="(item, index) in list.bilibili_list">
                 <span>{{ item.title }}</span>
             </view>
         </view>
     </view>
-    <view v-if="tab=='blog'" class="show-box flex-row content-center" lang="zh-CN">
-        <blog class="blog viewer"></blog>
-    </view>
-    <view v-if="tab=='netease'" class="show-box flex-row content-center" lang="zh-CN">
-        <netease class="netease viewer"></netease>
+    <view v-else class="show-box flex-row content-center" lang="zh-CN">
+        <component :is="currentTabComponent" class="viewer" :class="tab"></component>
     </view>
 </template>
 <style scoped>
