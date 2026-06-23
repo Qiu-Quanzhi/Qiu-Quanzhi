a4ff8d22a315c7e26913edf50fe3ea6969b46942
Qiu-Quanzhi <i@qqzhi.cc>
2026-06-23 15:22:24 +0800

    refactor: 将二维码图片归入 public/assets/qrcodes/

--- src/data.ts
+++ src/data.ts
@@ -9,8 +9,8 @@ const info = {
     timeZone: "Asia/Shanghai",
     socialMedias: [
         { id: "mail", url: "mailto:i@qqzhi.cc", info: "i@qqzhi.cc" },
-        { id: "Weixin", url: "/WX.png", mode: "embed", info: "Qiu-Qz" },
-        { id: "QQ", url: "/QQ.png", mode: "embed", info: "2766468782" },
+        { id: "Weixin", url: "/assets/qrcodes/WX.png", mode: "embed", info: "Qiu-Qz" },
+        { id: "QQ", url: "/assets/qrcodes/QQ.png", mode: "embed", info: "2766468782" },
         { id: "blog", url: "https://home.qqzhi.cc" },
         { id: "Github", url: "https://github.com/Qiu-Quanzhi" },
         { id: "Bilibili",url: "https://space.bilibili.com/1036651852" },
